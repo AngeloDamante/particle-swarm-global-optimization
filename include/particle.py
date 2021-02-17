c7168f68d471b7d5344ce8b21a94940e94c8895c
AngeloDamante <angelo.damante16@gmail.com>
2021-02-17 01:32:41 +0100

    class particle modified

--- include/particle.py
+++ include/particle.py
@@ -7,3 +7,17 @@ class Particle(object):
     def __init__(self, dimension, num_iterations):
         super(Particle, self).__init__()
         self.position = np.empty((num_iterations, dimension))
+        self.velocity = np.empty((num_iterations, dimension))
+        self.best_position = np.empty((num_iterations, dimension))
+
+        self.fitness = None
+        self.best_fitness = None
+
+    def update_position():
+        pass
+
+    def update_velocity():
+        pass
+
+    def evaluate():
+        pass
--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from particle import Particle
 if __name__ == '__main__':
     num_dimension = 2
     num_iterations = 30
+    p = Particle(num_dimension, num_iterations)
+
     # PSO(cost_function, num_dimension, )
     # population_generator()
-    p = Particle(num_dimension, num_iterations)
