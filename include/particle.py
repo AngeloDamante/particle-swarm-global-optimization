51c0c722359e396f8cbb5f5c0ab73566d3aecdb6
AngeloDamante <angelo.damante16@gmail.com>
2021-05-06 10:14:20 +0200

    Added docstring

--- include/particle.py
+++ include/particle.py
@@ -3,9 +3,22 @@ from random import random
 
 
 class Particle(object):
-    """docstring for Particle."""
+    """Implements Particle object with its property and activities.
+
+    Attributes:
+        position(ndarray): x-vector of particle (2x1)
+        velocity(ndarray): v-vector of particle (2x1)
+        local_best(ndarray): p-vector of particle (2x1)
+
+    """
 
     def __init__(self, bounds):
+        """Initilizes position and velocity of a particle.
+
+        Args:
+            bounds(ndarray): bounds per axis (nx2), with n-dimensions
+
+        """
         super(Particle, self).__init__()
 
         n_dim = len(bounds)
@@ -18,25 +31,52 @@ class Particle(object):
     # end
 
     def get_position(self):
+        """A simply get method.
+
+        Returns:
+            position(ndarray): with shape (2, 1)
+
+        """
         position = self.position.copy()
         return position
     # end
 
     def get_velocity(self):
+        """A simply get method.
+
+        Returns:
+            velocity(ndarray): with shape (2, 1)
+        """
         velocity = self.velocity.copy()
         return velocity
     # end
 
     def get_local_best(self):
+        """A simply get method.
+
+        Returns:
+            local_best(ndarray): with shape (2, 1)
+
+        """
         p = self.local_best.copy()
         return p
     # end
 
     def update_position(self):
+        """In according with PSO theory, this method updates position."""
         self.position += self.velocity
     # end
 
     def update_velocity(self, w, c1, c2, global_best):
+        """In according with PSO theory, this method updates velocity.
+
+        Args:
+            w(double): inertial constant
+            c1(double): cognitive constant
+            c2(double): social constant
+            global_best(ndarray): best global position of the swarm
+
+        """
         inertial_term = w * self.velocity
         cognitive_term = c1 * random() * (self.local_best - self.position)
         social_term = c2 * random() * (global_best - self.position)
@@ -45,6 +85,12 @@ class Particle(object):
     # end
 
     def evaluate_local_best(self, cost_function):
+        """Compute the local best as the minimum value taken by the cost function.
+
+        Args:
+            cost_function(function): to evaluate particle's position
+
+        """
         value = cost_function(self.position)
         if value < cost_function(self.local_best):
             self.local_best = self.position.copy()
--- include/swarm.py
+++ include/swarm.py
@@ -4,9 +4,24 @@ from particle import Particle
 
 
 class Swarm(object):
-    """docstring for Particle."""
+    """Implements Swarm with its particles and actions.
+
+    Attributes:
+        num_particles(int): number of particles for the swarm
+        dim(int): n-dimensions
+        particles(list): instantiated particles
+        global_best(ndarray): best position for the swarm (2x1)
+
+    """
 
     def __init__(self, num_particles, bounds):
+        """ Randomly initializes the population.
+
+        Args:
+            num_particles(int): number of particles for the swarm
+            bounds(ndarray): bounds per axis (nx2), with n-dimensions
+
+        """
         super(Swarm, self).__init__()
 
         self.num_particles = num_particles
@@ -22,11 +37,24 @@ class Swarm(object):
     # end
 
     def get_global_best():
+        """A simply get method.
+
+        Returns:
+            global_best(ndarray): with shape (2, 1)
+
+        """
+
         p_star = self.global_best.copy()
         return p_star
     # end
 
     def find_global_best(self, cost_function):
+        """Compute global best for the entire swarm.
+
+        Args:
+            cost_function(function): fitness function to evaluate positions
+
+        """
         g = cost_function(self.global_best)
         for i in range(0, self.num_particles):
             if g > cost_function(self.particles[i].local_best):
@@ -36,6 +64,12 @@ class Swarm(object):
     # end
 
     def get_particles_position(self):
+        """A simply get method that return all position of swarm's particles.
+
+        Returns:
+            positions(ndarray): with shape (num_particles, dim)
+
+        """
         actual_position = np.zeros((self.num_particles, self.dim))
         for i in range(0, self.num_particles):
             actual_position[i] = np.transpose(self.particles[i].get_position())
@@ -44,6 +78,12 @@ class Swarm(object):
     # end
 
     def get_particles_velocity(self):
+        """A simply get method that return all velocity of swarm's particles.
+
+        Returns:
+            velocities(ndarray): with shape (num_particles, dim)
+
+        """
         actual_velocity = np.zeros((self.num_particles, self.dim))
         for i in range(0, self.num_particles):
             actual_velocity[i] = np.transpose(self.particles[i].get_velocity())
@@ -52,6 +92,12 @@ class Swarm(object):
     # end
 
     def get_particles_local_best(self):
+        """A simply get method that return all individual best of particles.
+
+        Returns:
+            local_best(ndarray): with shape (num_particles, dim)
+
+        """
         local_best = np.zeros((self.num_particles, self.dim))
         for i in range(0, self.num_particles):
             local_best[i] = np.transpose(self.particles[i].get_local_best())
@@ -60,6 +106,12 @@ class Swarm(object):
     # end
 
     def get_state(self):
+        """A simply get method that return state of swarm.
+
+        Returns:
+            state(list): [positions | velocities | local_best | global_best]
+
+        """
         positions = self.get_particles_position()
         velocities = self.get_particles_velocity()
         local_best = self.get_particles_local_best()
@@ -70,6 +122,14 @@ class Swarm(object):
     # end
 
     def move(self, inertia, cognitive, social):
+        """In according to PSO theory, move the swarm following two equations.
+
+        Args:
+            inertia(double): inertia constant
+            cognitive(double): cognitive constant
+            social(double): cognitive constant
+
+        """
         w, c1, c2 = inertia, cognitive, social
 
         for i in range(0, self.num_particles):
@@ -79,12 +139,33 @@ class Swarm(object):
     # end
 
     def compute_local_best(self, cost_function):
+        """To evaluate all position and update individual best of swarm.
+
+        This method updates the individual bests simply by comparing the
+        current position and the previous individual best with the cost
+        function, in according to PSO standard theory.
+
+        Args:
+            cost_function(function): cost function to evaluate swarm's position.
+
+        """
         for i in range(0, self.num_particles):
             self.particles[i].evaluate_local_best(cost_function)
         # end
     # end
 
     def find_local_best(self, local_search_method, cost_function, initial_step=1):
+        """To implement memetic variant to standard PSO.
+
+        This method, equips PSO with a local_search_method to improve
+        perfomances of PSO.
+
+        Args:
+            local_search_method(function): local_search_method used
+            cost_function(function): cost function to minimize
+            initial_step(double): initial step for local_search_method
+
+        """
         for i in range(0, self.num_particles):
             x = self.particles[i].position
             self.particles[i].local_best = local_search_method(
